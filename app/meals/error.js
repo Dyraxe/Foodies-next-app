a1ee0e42a96dac443b5f1b8dceb4668160b4db20
Dyraxe <phdja25@gmail.com>
2024-05-18 19:06:07 -0300

    Handle errors

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function ErrorPage({ error }) {
+  return (
+    <main className="error">
+      <h1>Huh? Something went wrong.</h1>
+      <p>please try again in a few seconds!</p>
+    </main>
+  );
+}
