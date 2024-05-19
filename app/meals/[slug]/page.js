1aa9768d39ef8f053cbd248defea657baccca54b
Dyraxe <phdja25@gmail.com>
2024-05-19 11:41:49 -0300

    build image picker, add share meals form

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import { notFound } from "next/navigation";
 import classes from "./page.module.css";
 
 import { getMeal } from "@/lib/meals";
 export default function MealsDetailsPage({ params: { slug } }) {
   const meal = getMeal(slug);
+
+  if (!meal) {
+    notFound();
+  }
 
   meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
---
+++ components/meals/image-picker.js
@@ -0,0 +1,64 @@
+"use client";
+import Image from "next/image";
+import { useRef, useState } from "react";
+
+import classes from "./image-picker.module.css";
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState(null);
+
+  // ref is similar to the query selector
+  const imageInput = useRef();
+
+  function handlePickClick() {
+    // forward the click, from the button, to the input, since the input was hidden from the UI, in order to use a controlled button
+
+    imageInput.current.click();
+  }
+
+  function handleImageChange(event) {
+    // to allow for multiple files pick, use the "multiple" property on the input
+
+    const [file] = event.target.files;
+    if (!file) return setPickedImage(null);
+
+    // lets read the file stored in the browser, works asynchronously
+    const fileReader = new FileReader();
+
+    // sets callback to be called when the files load
+    fileReader.onload = () => {
+      // the image will be available through prototype.result
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+    // the result will be a data url, which works as src's value
+  }
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image src={pickedImage} fill alt="Image selected by the user" />
+          )}
+        </div>
+        <input
+          ref={imageInput}
+          id={name}
+          name={name}
+          className={classes.input}
+          type="file"
+          accept="image/png, image/jpeg"
+          onChange={handleImageChange}
+        />
+        <button
+          onClick={handlePickClick}
+          type="button"
+          className={classes.button}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
